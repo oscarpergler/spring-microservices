3d2e13d784e78e0f760c022e7838a4430bbb8d18
Oscar <oscarpergler@gmail.com>
2024-04-06 16:57:15 +0200

    configured post and timeline for AOT

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 SPRING_BOOT_PROJECT_DIR=$(pwd)
-MODULES=("gateway" "post" "timeline" "user")
+MODULES=("post" "timeline" "user")
 
 # Function to compile and create Docker image for a module
 compile_and_build_module() {
     local module=$1
-    echo "Natively compiling and creating Docker image for $module... (5-15 minutes depending on your hardware)"
+    echo "Natively compiling and creating Docker image for $module... (3-10 minutes depending on your hardware)"
     cd "$SPRING_BOOT_PROJECT_DIR/$module" || exit
     ./mvnw clean
     ./mvnw -Pnative spring-boot:build-image
@@ -17,5 +17,11 @@ compile_and_build_module() {
 for module in "${MODULES[@]}"; do
     compile_and_build_module "$module"
 done
+
+echo "Compiling and creating Docker image for gateway..."
+cd "$SPRING_BOOT_PROJECT_DIR/gateway" || exit
+./mvnw clean install
+docker build -t gateway:1.0.0-SNAPSHOT .
+echo "Docker image for $module has been built successfully."
 
 echo "All Docker images have been built successfully."
