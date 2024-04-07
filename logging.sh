3254c8f983a18205f0b104119df9ba11e17bb15d
Oscar <oscarpergler@gmail.com>
2024-04-07 16:57:55 +0200

    logging script and logs

--- logging.sh
+++ logging.sh
@@ -1,10 +1,69 @@
 #! /bin/bash
 
-# decide how many times to iterate the following code:
+# Check if container name is provided as argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <container_name>"
+    exit 1
+fi
 
-# curl to localhost:8080/posts/actuator/health/liveness to see if the post service is ready for traffic
+# Number of iterations
+ITERATIONS=30
 
-# if it is ready for traffic, collect logs and append them to a local text file:
-# docker logs [container_name] | findstr "process runnig forn" > log.txt
+# Container name provided as argument
+CONTAINER_NAME="$1"
 
-# docker restart [container_name]
+# Log file
+LOG_FILE="$CONTAINER_NAME.txt"
+
+touch "$LOG_FILE"
+
+case "$CONTAINER_NAME" in
+    "posts")
+        echo "Posts-service is mapped to localhost:8081"
+        PORT=8081
+        ;;
+    "timeline")
+        echo "Posts-service is mapped to localhost:8082"
+        PORT=8082
+        ;;
+    "users")
+        echo "Posts-service is mapped to localhost:8083"
+        PORT=8083
+        ;;
+    *)
+        echo "$CONTAINER_NAME is not a valid container"
+        exit
+        ;;
+esac
+
+# Service endpoint
+SERVICE_ENDPOINT="http://localhost:$PORT/actuator/health/liveness"
+
+for ((i=1; i<=$ITERATIONS; i++))
+do
+    echo "$CONTAINER_NAME: Iteration $i"
+
+    # Check service readiness
+    response=$(curl -s -o /dev/null -w "%{http_code}" $SERVICE_ENDPOINT)
+
+    while [ $response -ne 200 ]; do
+      echo "$CONTAINER_NAME-service is not ready. $response"
+      sleep 1
+      response=$(curl -s -o /dev/null -w "%{http_code}" $SERVICE_ENDPOINT)
+    done
+
+    if [ $response -eq 200 ]; then
+        echo "Service is ready. Collecting logs..."
+
+        # Restart container
+        echo "Restarting container $CONTAINER_NAME..."
+        docker restart $CONTAINER_NAME
+    fi
+
+    sleep 5  # Adjust sleep duration as needed
+done
+
+# Collect logs
+docker logs $CONTAINER_NAME | grep "process running for" >> $LOG_FILE
+
+echo "Script completed"
