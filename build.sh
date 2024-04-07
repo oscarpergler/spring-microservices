962d2f5f22bb192f683d5138351457da6ff365b8
Oscar <oscarpergler@gmail.com>
2024-04-07 18:34:13 +0200

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 SPRING_BOOT_PROJECT_DIR=$(pwd)
-MODULES=("post" "timeline" "user")
+MODULES=("posts" "timeline" "users")
 
 # Function to compile and create Docker image for a module
 compile_and_build_module() {
@@ -21,7 +21,7 @@ done
 echo "Compiling and creating Docker image for gateway..."
 cd "$SPRING_BOOT_PROJECT_DIR/gateway" || exit
 ./mvnw clean install
-docker build -t gateway:1.0.0-SNAPSHOT .
+docker build -t gateway:0.0.1-SNAPSHOT .
 echo "Docker image for $module has been built successfully."
 
 echo "All Docker images have been built successfully."
