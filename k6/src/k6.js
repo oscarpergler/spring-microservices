39e5621dbd1a085684059eee76c560e5bcccef51
b21oscpe <oscarpergler@gmail.com>
2024-04-22 10:21:25 +0200

    explanatory comments

--- k6/src/k6.js
+++ k6/src/k6.js
@@ -1,12 +1,17 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 export const options = {
-  vus: 100,
-  duration: '60s',
+  vus: 100, // 100 virtual users
+  duration: '60s', // about 6000 requests
 };
 export default function () {
   http.get('http://gateway:8080/posts/all');
   sleep(1);
 }
 
-// docker run --rm --network=microservices -v C:/Users/oscar/Documents/exjobb/compiled-javaservices/k6/src:/src -i grafana/k6 run /src/k6.js
+/**
+ You must first pull the k6 image: docker pull grafana/k6
+ Then run the following command to run the test:
+
+ docker run --rm --network=microservices -v C:/Users/oscar/Documents/exjobb/compiled-javaservices/k6/src:/src -i grafana/k6 run /src/k6.js
+ **/
