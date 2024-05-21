411bdbc811f61672cecac19fecf091fb83079172
Oscar <oscarpergler@gmail.com>
2024-05-21 22:12:55 +0200

    Updated k6.js and added config for h2-console

--- k6/src/k6.js
+++ k6/src/k6.js
@@ -1,12 +1,16 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+
 export const options = {
-    vus: 100, // 100 virtual users
-    duration: '60s', // about 6000 requests
+    vus: 50, // 50 virtual users
+    duration: '100s', // sums up to about 5000 requests
 };
+
+/*
+ Through gateway: http://gateway:8080/posts
+ Directly to service: http://posts:8081/posts
+*/
 export default function () {
-    http.get('http://gateway:8080/posts/all');
-    sleep(1);
+    http.get('http://posts:8081/posts');
 }
 
 /**
@@ -14,5 +18,5 @@ export default function () {
 
  adjust the directory as needed:
 
- docker run --rm --network=microservices -v C:/Users/oscar/Projekt/compiled-javaservices/k6/src:/src -i grafana/k6 run /src/k6.js
+ docker run --rm --network=microservices -v C:/Users/oscar/Documents/GitHub/spring-microservices/k6/src:/src -i grafana/k6 run /src/k6.js
  **/
