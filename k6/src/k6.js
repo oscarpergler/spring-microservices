4bbcc818202d01276b12550a99b50f86d1624454
Oscar <oscarpergler@gmail.com>
2024-05-22 18:41:30 +0200

    Update k6.js

--- k6/src/k6.js
+++ k6/src/k6.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-    vus: 1,
-    duration: '1000s',
+    vus: 5,
+    duration: '4000s',
 };
 
 /*
@@ -11,7 +11,7 @@ export const options = {
  Directly to service: http://posts:8081/posts
 */
 export default function () {
-    http.get('http://posts:8081/posts');
+    http.get('http://gateway:8080/posts');
     sleep(1);
 }
 
