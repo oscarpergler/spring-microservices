032da7e77a1ac893eaba866ca3588933b3b970b3
Oscar <oscarpergler@gmail.com>
2024-05-24 00:26:45 +0200

    k6 data jit

--- k6/src/k6.js
+++ k6/src/k6.js
@@ -2,23 +2,18 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-    vus: 5,
-    duration: '4000s',
+    vus: 1,
+    duration: '2000s',
 };
 
 /*
- Through gateway: http://gateway:8080/posts
- Directly to service: http://posts:8081/posts
+ Through gateway: http://localhost:8080/posts
+ Directly to service: http://localhost:8081/posts
 */
 export default function () {
-    http.get('http://gateway:8080/posts');
+    http.get('http://localhost:8081/posts');
     sleep(1);
 }
 
-/**
- You must first pull the k6 image: docker pull grafana/k6
-
- adjust the directory as needed:
-
- docker run --rm --network=microservices -v C:/Users/oscar/Documents/GitHub/spring-microservices/k6/src:/src -i grafana/k6 run /src/k6.js
- **/
+// install k6 -->
+// run: k6 run k6.js --out csv=[name of file]].csv
